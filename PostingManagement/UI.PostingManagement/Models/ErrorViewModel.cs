1b6b11e4c4a08a09f919b927828d191705ccfac9
shankar <shankarkadam54@gmail.com>
2022-10-31 11:04:54 +0530

    added UI project

---
+++ PostingManagement/UI.PostingManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UI.PostingManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
